0ffe88717134a2f3830258f7a62da23f0b1f2a86
Fredrik Engstrand <fredrik@engstrand.nu>
2020-07-22 12:49:39 +0200

    Fixed line number alignments

--- main.js
+++ main.js
@@ -4,13 +4,10 @@ var content = document.getElementById('content');
 var gutter = document.getElementById('gutter');
 var scrollBlock = document.getElementById('scroll');
 
-content.scrollTo(0, 0);
-gutter.scrollTo(0, 0);
-
 function generateLineNumbers() {
   var amount = Math.ceil(content.scrollHeight / lineHeight);
 
-  for (var i = 1; i < amount; i++) {
+  for (var i = 1; i <= amount; i++) {
     var element = document.createElement('div');
     element.innerText = i;
     element.classList.add('line-number');
@@ -22,8 +19,8 @@ generateLineNumbers();
 
 function scrollContent(amount) {
   if (
-    (content.scrollTop + content.clientHeight + lineHeight) > content.scrollHeight ||
-    (gutter.scrollTop + gutter.clientHeight + lineHeight) > gutter.scrollHeight &&
+    ((content.scrollTop + content.clientHeight + lineHeight + 3) >= content.scrollHeight ||
+    (gutter.scrollTop + content.clientHeight + lineHeight) >= content.scrollHeight) &&
     amount > 0
   ) {
     // We only want to sroll even steps
@@ -32,9 +29,6 @@ function scrollContent(amount) {
 
   content.scrollBy(0, amount);
   gutter.scrollBy(0, amount);
-
-  console.log(content.scrollTop);
-  console.log(gutter.scrollTop);
 
   if (content.scrollTop === 0) {
     scrollBlock.innerText = '0%';
