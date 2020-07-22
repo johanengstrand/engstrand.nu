3610e8775a51a9dae54abef95c7ab8d3f4405cec
Fredrik Engstrand <fredrik@engstrand.nu>
2020-07-22 21:54:26 +0200

    Added tab navigation keybindings

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const lineHeight = 20;
+const tabs = ['index.html', 'johan.html', 'fredrik.html', 'contact.html'];
 const modes = {
   normal: 'NORMAL',
   command: 'COMMAND',
@@ -14,6 +15,8 @@ var keyBlock = document.getElementById('keybinding-display');
 var fileBlock = document.getElementById('file');
 
 var currentMode = modes.command;
+var currentTab = null;
+var previousTab = null;
 var keypresses = '';
 
 function generateLineNumbers() {
@@ -45,6 +48,7 @@ function updateFilenameBlock() {
     filename = 'index.html';
   }
 
+  currentTab = tabs.indexOf(filename) + 1; // Tabs start at 1
   fileBlock.innerText = filename;
 }
 
@@ -88,7 +92,7 @@ function scrollContentToBottom() {
   main.scrollTo(0, main.scrollHeight);
 }
 
-function scrollContent(amount, repeats) {
+function scrollContent(amount) {
   if (amount > 0) {
     var treshold = main.scrollTop + (main.clientHeight - (main.clientHeight % lineHeight) + lineHeight);
     if (treshold > main.scrollHeight) {
@@ -98,9 +102,9 @@ function scrollContent(amount, repeats) {
   }
 
   if (keypresses.length > 0) {
-    const repeatAmount = parseInt(keypresses);
-    if (!Object.is(repeatAmount, NaN)) {
-      amount *= repeatAmount;
+    // TODO: I think parseInt() simply ignores letters, which is not ideal
+    if (isNumber(keypresses)) {
+      amount *= parseInt(keypresses);
       resetKeyBlock();
     } else {
       // If we have previous keypresses that are not numbers, it is an invalid command.
@@ -122,6 +126,17 @@ function updateKeyBlock(key) {
   keyBlock.innerText = keypresses;
 }
 
+function goToTab(tab) {
+  if (tab > 0 && tab <= tabs.length) {
+    console.log('Go to tab: ', tab);
+  } else {
+    console.error('Invalid tab id: ', tab);
+  }
+
+  previousTab = currentTab;
+  currentTab = tab;
+}
+
 function commandModeKeybindings(key) {
   switch (key) {
     case 'Enter':
@@ -130,13 +145,14 @@ function commandModeKeybindings(key) {
   }
 }
 
+function isNumber(keys) {
+  return !Object.is(parseInt(keys), NaN);
+}
+
 function normalModeKeybindings(key) {
   // Check if the key is a number and add it to the repeat count.
   // This is used when we want to repeat a binding multiple times, e.g '10k'.
-  if (
-    !Object.is(parseInt(key), NaN) &&
-    (keypresses.length === 0 || !Object.is(parseInt(keypresses), NaN))
-  ) {
+  if (isNumber(key) && (keypresses.length === 0 || isNumber(keypresses))) {
     updateKeyBlock(key);
     return;
   }
@@ -163,6 +179,16 @@ function normalModeKeybindings(key) {
         scrollContentToBottom();
       }
       break;
+    case 't':
+      const tabNumber = keypresses.substr(0, keypresses.length - 1);
+      if (keypresses.slice(-1) === 'g') {
+        if (keypresses.length > 1 && isNumber(tabNumber)) {
+          goToTab(tabNumber);
+        } else {
+          previousTab !== null && goToTab(previousTab);
+        }
+      }
+      break;
     case 'Shift':
       reset = false;
       break;
@@ -171,8 +197,7 @@ function normalModeKeybindings(key) {
       break;
   }
 
-  // Reset the key-sequence if no binding was found for the pressed key
-  // or if we manually trigger a reset based on some logic.
+  // Reset the key-sequence if no binding was found for the pressed key and we did not prevent it.
   if (reset || !foundBinding) {
     resetKeyBlock();
   }
