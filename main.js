be8ae2cf8d3d3597d43b9c26b4ae0d7cf699671d
Fredrik Engstrand <fredrik@engstrand.nu>
2020-07-22 17:05:11 +0200

    Added command mode and input. Updated theme

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-var lineHeight = 20;
+const lineHeight = 20;
+const modes = {
+  normal: 'NORMAL',
+  command: 'COMMAND',
+};
 
 var main = document.getElementById('content-wrapper');
 // var content = document.getElementById('content');
 var gutter = document.getElementById('gutter');
 var scrollBlock = document.getElementById('scroll');
+var modeBlock = document.getElementById('mode');
+var commandInput = document.getElementById('command-input');
+var fileBlock = document.getElementById('file');
+var currentMode = modes.command;
 
 function generateLineNumbers() {
   var amount = Math.ceil(main.scrollHeight / lineHeight);
@@ -16,7 +24,7 @@ function generateLineNumbers() {
   }
 }
 
-function adjustContentScrollHeight() {
+function updateContentScrollHeight() {
   var remainder = main.scrollHeight % lineHeight;
 
   if (remainder !== 0) {
@@ -26,15 +34,43 @@ function adjustContentScrollHeight() {
   }
 }
 
-generateLineNumbers();
-adjustContentScrollHeight();
+function updateFilenameBlock() {
+  const path = window.location.pathname
+  const filename = path.substring(path.lastIndexOf('/') + 1);
+  fileBlock.innerText = filename;
+}
+
+function initialize() {
+  generateLineNumbers();
+  updateFilenameBlock();
+  updateMode(modes.normal);
+  updateContentScrollHeight();
+}
+
+function updateMode(mode) {
+  modeBlock.innerText = mode;
+  currentMode = mode;
+
+  if (mode === modes.normal) {
+    commandInput.value = '';
+    commandInput.blur();
+  }
+}
+
+function startCommandInput() {
+  commandInput.focus();
+}
+
+function executeCommand() {
+  var command = commandInput.value.substr(1);
+  console.log(command);
+  updateMode(modes.normal);
+}
 
 function scrollContent(amount) {
   if (amount > 0) {
     var treshold = main.scrollTop + (main.clientHeight - (main.clientHeight % lineHeight) + lineHeight);
     if (treshold > main.scrollHeight) {
-      console.log(treshold);
-      console.log(main.scrollHeight);
       // We only want to sroll even steps
       return;
     }
@@ -49,8 +85,19 @@ function scrollContent(amount) {
   }
 }
 
-window.addEventListener('keydown', (e) => {
-  switch(e.key) {
+function commandModeKeybindings(key) {
+  switch (key) {
+    case ':':
+      startCommandInput();
+      break;
+    case 'Enter':
+      commandInput.value !== '' && commandInput.value !== ':' && executeCommand();
+      break;
+  }
+}
+
+function normalModeKeybindings(key) {
+  switch (key) {
     case 'j':
       scrollContent(lineHeight);
       break;
@@ -58,4 +105,16 @@ window.addEventListener('keydown', (e) => {
       scrollContent(-lineHeight);
       break;
   }
+}
+
+window.addEventListener('keydown', (e) => {
+  const key = e.key;
+
+  if (key === 'Escape' || key === 'CapsLock') {
+    currentMode === modes.command ? updateMode(modes.normal) : updateMode(modes.command);
+  } else {
+    currentMode === modes.normal ? normalModeKeybindings(key) : commandModeKeybindings(key);
+  }
 });
+
+initialize();
