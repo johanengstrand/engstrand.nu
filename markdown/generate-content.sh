36bc1e147523b493c84524b6086b676aba2ec7a3
Johan Engstrand <johan@engstrand.nu>
2020-07-24 19:01:25 +0200

    sed substitution for ascii art

--- markdown/generate-content.sh
+++ markdown/generate-content.sh
@@ -3,6 +3,7 @@
 for f in *.md
 do
   FILENAME="${f%%.md}"
-  pandoc -i $f --filter pandoc-run-filter -o ../content/$FILENAME.html
+  sed -ri 's/^(\@ascii) (.*)/printf "<pre data-type=\"\1\">"; toilet -f mono12 \2; printf "<pre data-type=\"\1\">"/e' $f
+  pandoc -i $f -f gfm -o ../content/$FILENAME.html
 done
 
