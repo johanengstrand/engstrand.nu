1d8ee323fcb72752ac3b985eed1ce6605b994275
Johan Engstrand <johan@engstrand.nu>
2020-07-25 15:46:54 +0200

    changed macro for font-awesome

--- markdown/generate-content.sh
+++ markdown/generate-content.sh
@@ -5,7 +5,7 @@ do
   FILENAME="${f%%.md}"
   cp $f /tmp/current.md
   sed -ri 's/^(\@ascii) (.*)/echo "<pre data-type=\\"\1\\">"; echo "<pre>"; toilet -f mono12 \2; echo "<\/pre>"; echo "<h1>\2<\/h1>"; echo "<\/pre>"/e' /tmp/current.md
-  sed -ri 's/^(\@fa) (.*)/cat ..\/font-awesome\/svgs\/\2.svg/e' /tmp/current.md
+  sed -ri 's/^(\@icon) (.*)/cat ..\/font-awesome\/svgs\/\2.svg/e' /tmp/current.md
   pandoc -i /tmp/current.md -f gfm -o ../content/$FILENAME.html
 done
 
