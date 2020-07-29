4a8a8bec18e2a34a347e5c8d63827ab4938bb49e
Fredrik Engstrand <fredrik@engstrand.nu>
2020-07-30 00:07:50 +0200

    Removed unused javascript. Added HTML templates

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,7 @@
 const lineHeight = 20;
 const mobileBreakpoint = 780;
 const modes = { normal: 'NORMAL', command: 'COMMAND' };
-const tabs = [ 'index.html', 'johan.html', 'fredrik.html', 'pywalfox.html', 'contact.html' ];
-const themeData = [
-  { variable: '--wallpaper', attribute: 'data-wallpaper' },
-  { variable: '--color-border', attribute: 'data-color-border' },
-  { variable: '--color-background', attribute: 'data-color-background' },
-  { variable: '--color-background-light', attribute: 'data-color-background-light' },
-  { variable: '--color-primary', attribute: 'data-color-primary' },
-  { variable: '--color-secondary', attribute: 'data-color-secondary' },
-  { variable: '--color-default-text', attribute: 'data-color-default-text' },
-  { variable: '--color-accent-text', attribute: 'data-color-accent-text' },
-  { variable: '--color-secondary-text', attribute: 'data-color-secondary-text' },
-  { variable: '--color-content-text', attribute: 'data-color-content-text' },
-  { variable: '--color-line-number', attribute: 'data-color-line-number' },
-];
 
-const tabbar = document.getElementById('tabbar');
 const main = document.getElementById('content-wrapper');
 const content = document.getElementById('content');
 const gutter = document.getElementById('gutter');
@@ -24,14 +9,9 @@ const scrollBlock = document.getElementById('scroll');
 const modeBlock = document.getElementById('mode');
 const commandInput = document.getElementById('command-input');
 const keyBlock = document.getElementById('keybinding-display');
-const fileBlock = document.getElementById('file');
 
 var currentMode = modes.command;
 var keypresses = '';
-var currentTab = null;
-var previousTab = null;
-var selectedTabElement = null;
-var tabElements = {};
 var ticking = false;
 
 // https://remysharp.com/2010/07/21/throttling-function-calls
@@ -46,98 +26,27 @@ function debounce(fn, delay) {
   };
 }
 
-function generateTabs() {
-  tabs.forEach((tab, index) => {
-    const tabId = index + 1;
-    const extensionStartIndex = tab.indexOf('.');
-    const tabName = tab.substr(0, extensionStartIndex);
-    const tabExtension = tab.substr(extensionStartIndex);
-    const element = document.createElement('button');
+function outerHeight(element) {
+  // https://stackoverflow.com/questions/10787782/full-height-of-a-html-element-div-including-border-padding-and-margin
+  const height = element.offsetHeight;
+  const style = window.getComputedStyle(element);
 
-    [tabId, tabName, tabExtension].forEach((data) => {
-      const span = document.createElement('span');
-      span.innerText = data;
-      element.appendChild(span);
-    });
-
-    element.classList.add('tab');
-    element.addEventListener('click', () => goToTab(tabId));
-
-    tabbar.appendChild(element);
-
-    // Add to list for faster access when switching tabs
-    tabElements[tabId] = element;
-  });
+  return ['top', 'bottom']
+    .map(side => parseInt(style[`margin-${side}`]))
+    .reduce((total, side) => total + side, height);
 }
 
-function updateSelectedTab() {
-  const selectedTab = tabElements[currentTab];
-
-  if (!selectedTab) {
-    return;
-  }
-
-  if (selectedTabElement !== null) {
-    selectedTabElement.classList.remove('tab-active');
-  }
-
-  selectedTab.classList.add('tab-active');
-  selectedTabElement = selectedTab;
-}
-
-function updateColorVariables() {
-  const template = content.querySelector('template');
-
-  if (template) {
-    document.body.classList.remove('default-theme');
-    themeData.forEach((obj) => {
-      document.body.style.setProperty(obj.variable, template.getAttribute(obj.attribute));
-    });
-  } else {
-    document.body.classList.add('default-theme');
-    document.body.style = null;
-  }
-}
-
-function openContentPage(path) {
-  // Fetches an html file and inserts the content into the page
-  fetch('content/' + path)
-    .then((response) => {
-      return response.text();
-    })
-    .then((html) => {
-      content.innerHTML = html;
-      updateColorVariables();
-      waitForMediaToLoad();
-      updateSelectedTab();
-      updateLineNumbers(); // Generate temporary line numbers
-      updateFilenameBlock();
-      applyLineHeightFixes();
-      scrollContentToTop();
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+function isNumber(keys) {
+  return !Object.is(parseInt(keys), NaN);
 }
 
 function adjustLineOverflow(elementHeight, callback) {
   // If adjustment is needed to align with the lines,
   // send the amount in pixels to callback for adjustment
   const remainder = elementHeight % lineHeight;
+
   if (remainder !== 0) {
     callback(remainder);
-  }
-}
-
-function applyLineHeightFixes() {
-  // Applies the line height fix to all other required elements except 'img' and 'video'
-  document.querySelectorAll('svg').forEach((element) => applyLineHeightFix(element.parentElement));
-}
-
-function applyLineHeightFix(parentElement) {
-  if (parentElement.tagName === 'P') {
-    // Fixes an issue where the p-tag would be a few pixels taller than the actual content
-    parentElement.style.lineHeight = '0px';
   }
 }
 
@@ -151,30 +60,11 @@ function applyLineHeightFix(parentElement, className='line-height-fix') {
 function updateMediaHeight(element) {
   adjustLineOverflow(element.clientHeight, (remainder) => {
     const parentElement = element.parentElement;
-    var newHeight = element.clientHeight - remainder;
+    const adjustedHeight = Math.max(lineHeight, element.clientHeight - remainder);
 
-    if (newHeight < lineHeight) {
-      newHeight = lineHeight;
-    }
-
-    if (parentElement.innerText.length > 0) {
-      applyLineHeightFix(parentElement, 'temporary-line-height-fix');
-    } else {
-      applyLineHeightFix(parentElement);
-    }
-
-    element.style.height = newHeight + 'px';
+    applyLineHeightFix(parentElement, parentElement.innerText.length > 0 ? 'temporary-line-height-fix' : null);
+    element.style.height = `${adjustedHeight}px`;
   });
-}
-
-function outerHeight(element) {
-  // https://stackoverflow.com/questions/10787782/full-height-of-a-html-element-div-including-border-padding-and-margin
-  const height = element.offsetHeight;
-  const style = window.getComputedStyle(element);
-
-  return ['top', 'bottom']
-    .map(side => parseInt(style[`margin-${side}`]))
-    .reduce((total, side) => total + side, height);
 }
 
 function calculateRealContentHeight() {
@@ -189,6 +79,7 @@ function calculateRealContentHeight() {
 
 function createLineNumberElement(number) {
   const element = document.createElement('div');
+
   element.innerText = number;
   element.classList.add('line-number');
 
@@ -196,6 +87,7 @@ function createLineNumberElement(number) {
 }
 
 function updateLineNumbers() {
+  // TODO: Optimze this function
   gutter.innerHTML = '';
 
   const contentHeight = calculateRealContentHeight();
@@ -214,7 +106,7 @@ function waitForMediaToLoad() {
 
   // Create a list of promises
   media.forEach((element) => {
-    const promise = new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, _) => {
       const listenEvent = element.tagName === 'VIDEO' ? 'loadedmetadata' : 'load';
       element.addEventListener('error', resolve); // we do not actually care about the error
       element.addEventListener(listenEvent, () => {
@@ -227,25 +119,6 @@ function waitForMediaToLoad() {
   });
 
   Promise.all(listeners).then(updateLineNumbers);
-}
-
-function getCurrentTabIdFromPathname() {
-  const path = window.location.pathname;
-  const tabIndex = tabs.indexOf(path.substring(path.lastIndexOf('/') + 1));
-
-  return tabIndex >= 0 ? (tabIndex + 1) : -1;
-}
-
-function initialize() {
-  const tabId = getCurrentTabIdFromPathname();
-
-  tabId !== -1 ? goToTab(tabId) : goToTab(1);
-  generateTabs();
-  updateMode(modes.normal);
-}
-
-function updateFilenameBlock() {
-  fileBlock.innerText = tabs[currentTab-1];
 }
 
 function updateMode(mode) {
@@ -262,7 +135,20 @@ function updateMode(mode) {
   }
 }
 
+function goToTab(tabId) {
+  const tab = document.querySelector(`.tab[data-id=${tabId}]`);
+
+  if (!tab) {
+    console.error(`Could not find tab with id: ${tabId}`);
+    return;
+  }
+
+  /* use 'window.location.href' to simulate clicking a link */
+  window.location.href = tab.href;
+}
+
 function executeCommand() {
+  // TODO: Add commands for command mode
   var command = commandInput.value.substr(1);
   console.log(command);
   updateMode(modes.normal);
@@ -308,9 +194,11 @@ function updateScrollBlock() {
 
   if (scrollAmount === 0) {
     resetScrollBlock();
-  } else {
-    scrollBlock.innerText = Math.abs(parseInt(scrollAmount / (scrollHeight - main.clientHeight) * 100)) + '%';
+    return;
   }
+
+  const scrollPercent = Math.abs(parseInt(scrollAmount / (scrollHeight - main.clientHeight) * 100));
+  scrollBlock.innerText = `${scrollPercent}%`;
 }
 
 function scrollContent(amount) {
@@ -333,25 +221,6 @@ function updateKeyBlock(key) {
   keyBlock.innerText = keypresses;
 }
 
-function goToTab(tab, saveHistory=true) {
-  if (tab <= 0 || tab > tabs.length) {
-    console.error('Invalid tab id: ', tab);
-    return;
-  }
-
-  const filename = tabs[tab-1];
-  if (tab !== currentTab && saveHistory) {
-    window.history.pushState({ tabId: tab }, '', filename);
-  }
-
-  // Update the global variables before opening the content page
-  // so that they can be used when the html has loaded.
-  previousTab = currentTab === null ? tab : currentTab;
-  currentTab = tab;
-
-  openContentPage(filename);
-}
-
 function commandModeKeybindings(key) {
   switch (key) {
     case 'Enter':
@@ -362,10 +231,6 @@ function commandModeKeybindings(key) {
       }
       break;
   }
-}
-
-function isNumber(keys) {
-  return !Object.is(parseInt(keys), NaN);
 }
 
 function normalModeKeybindings(key) {
@@ -400,12 +265,8 @@ function normalModeKeybindings(key) {
       break;
     case 't':
       const tabNumber = keypresses.substr(0, keypresses.length - 1);
-      if (keypresses.slice(-1) === 'g') {
-        if (keypresses.length > 1 && isNumber(tabNumber)) {
-          goToTab(tabNumber);
-        } else {
-          previousTab !== null && goToTab(previousTab);
-        }
+      if (keypresses.length > 1 && keypresses.slice(-1) === 'g' && isNumber(tabNumber)) {
+        goToTab(tabNumber);
       }
       break;
     case 'Shift':
@@ -434,17 +295,13 @@ function handleScrollEvent() {
   }
 }
 
-document.addEventListener('keydown', (e) => {
-  const key = e.key;
+function initialize() {
+  updateMode(modes.normal);
+}
 
-  if (key === 'Escape' || key === 'CapsLock') {
-    updateMode(modes.normal);
-  } else if (key === ':') {
-    updateMode(modes.command);
-  } else {
-    currentMode === modes.normal ? normalModeKeybindings(key) : commandModeKeybindings(key);
-  }
-});
+// Different elements are being scrolled based on the current device and window size
+window.addEventListener('scroll', handleScrollEvent);
+main.addEventListener('scroll', handleScrollEvent);
 
 window.addEventListener('resize', debounce(() => {
   /* Media height is calculated on load and if you resize the window,
@@ -456,8 +313,6 @@ window.addEventListener('resize', debounce(() => {
    */
   const media = document.querySelectorAll('img, video');
   media.forEach((element) => {
-    element.parentElement.style.height = null;
-    element.style.height = null;
     updateMediaHeight(element)
   });
 
@@ -465,27 +320,20 @@ window.addEventListener('resize', debounce(() => {
   updateLineNumbers();
 }, 250));
 
-window.addEventListener('popstate', (e) => {
-  var tabId = e.state.tabId;
+document.addEventListener('keydown', (e) => {
+  const key = e.key;
 
-  /* Try to fetch the target tabId from history state first.
-   * If it does not exists, try getting an index from the pathname.
-   * If no tab id can be found, display the index page
-   */
-  if (tabId) {
-    goToTab(tabId, false);
-  } else {
-    tabId = getCurrentTabIdFromPathname();
-    if (tabId !== -1) {
-      goToTab(tabId, false);
-    } else {
-      goToTab(1, false);
-    }
-  }
+  switch (key) {
+    case 'Escape': /* fallthrough */
+    case 'CapsLock':
+      updateMode(modes.normal);
+      break;
+    case ':':
+      updateMode(modes.command);
+      break;
+    default:
+      currentMode === modes.normal ? normalModeKeybindings(key) : commandModeKeybindings(key);
+      break;
 });
-
-// Different elements are being scrolled based on the current device and window size
-window.addEventListener('scroll', handleScrollEvent);
-main.addEventListener('scroll', handleScrollEvent);
 
 initialize();
